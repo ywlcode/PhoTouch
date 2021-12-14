fac297f03123aad61634ed74873e79555c099635
ywlcode <r5554333@163.com>
2021-12-14 11:59:56 +0800

    布局,导航栏...

--- assets/script/main.js
+++ assets/script/main.js
@@ -13,6 +13,30 @@ $(window).scroll(function(){
         kk += mm;
         console.log(window.scrollY);
     };
-})
+});
 
-$()
+var flag = 1;
+
+$(document).ready(function(){
+    $("#z2").click(function(){
+        if(flag == 1)
+        {
+            $(".content").css("width","calc(100% - 180px)");
+            $(".content").css("left","180px");
+            $(".side").css("width","180px");
+            $(".box span").css("opacity","1");
+            $("#z2 i").addClass("icon-fanhuijiantou");
+            $("#z2 i").removeClass("icon-chufadaodaxiao");
+            flag = 0;
+        }
+        else {
+            $(".content").css("width","calc(100% - 78px)");
+            $(".content").css("left","78px");
+            $(".side").css("width","78px");
+            $(".box span").css("opacity","0");
+            $("#z2 i").addClass("icon-chufadaodaxiao");
+            $("#z2 i").removeClass("icon-fanhuijiantou");
+            flag = 1;
+        }
+    })
+});
